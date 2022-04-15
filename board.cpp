0e23e4e3acad660a2e3c9622d0643bcb9fa1ca47
Siddharth Soni <sonisiddharth2003@gmail.com>
2022-04-15 10:29:50 +0530

    [.] renamed game.h->game.hpp, board.h->board.hpp

--- board.cpp
+++ board.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <math.h>
-#include "board.h"
+#include "board.hpp"
 
 //PRIVATE FUNCTIONS
 void BitBoard :: initVariables()
--- game.cpp
+++ game.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "game.h"
+#include "game.hpp"
 
 const int MAX_DEPTH = 10;
 
